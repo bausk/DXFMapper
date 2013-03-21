b672ac888318e8a8f299f88e0804733a11c7e237
Alex Bausk at Gibraltar <bauskas@gmail.com>
2013-03-22 00:24:09 +0200

    Before trying direct Drawing object

--- PyDfxCoder_VS10/PyDxfReadFile.py
+++ PyDfxCoder_VS10/PyDxfReadFile.py
@@ -9,58 +9,8 @@ import dxfgrabber
 from sys import argv
 from configobj import ConfigObj
 from validate import Validator
+from dxfgrabber.drawing import Drawing
 
-
-def dxfString():
-    return """  0
-    TEXT
-      5
-    470
-    102
-    {ACAD_XDICTIONARY
-    360
-    471
-    102
-    }
-    330
-    1F
-    100
-    AcDbEntity
-      8
-    0
-    100
-    AcDbText
-     10
-    17.0
-     20
-    17.0
-     30
-    0.0
-     40
-    3.0
-      1
-    TEXT
-      7
-    Notes
-    100
-    AcDbText
-    1001
-    AcadAnnotative
-    1000
-    AnnotativeData
-    1002
-    {
-    1070
-         1
-    1070
-         1
-    1002
-    }
-    1001
-    AcadAnnoPO
-    1070
-         1
-    """
 def readSettings():
     v = Validator()
     spec = ConfigObj("Parameters.spec", encoding = "UTF8", list_values=False)
@@ -68,15 +18,21 @@ def readSettings():
     config.validate(v)
     return config
 
+def readSettingsKey(Section, Key, settings):
+    if Section in settings and Key in settings[Section]:
+        return settings[Section][Key]
+    else:
+        return False
+
 def main():
     script, filename = argv
     settings = readSettings()
-    for setting in settings:
-        print settings[setting][]
-        
-    astring = unicode(open("../DXF Examples/" + filename + ".dxf").read())
-    tags = ClassifiedTags.fromtext(unicode(open("../DXF Examples/" + filename + ".dxf").read()))
-    #dxf = dxfgrabber.readfile()
+    InputFile = readSettingsKey("Input", "File", settings) or "Default.dxf"
+    OutputFile = readSettingsKey("Output", "File", settings) or "Default.lir"
+
+    #astring = unicode(open("../DXF Examples/" + filename + ".dxf").read())
+    tags = ClassifiedTags.fromtext(unicode(open(InputFile).read()))
+    
     print tags
 
 
